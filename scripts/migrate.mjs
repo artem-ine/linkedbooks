68c0e9de7dfb2f34c184511035a1ff698237580c
zeke <ezekiel.ravidat@gmail.com>
2025-04-25 18:20:50 +0200

    migration token

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -5,7 +5,7 @@ import path from "path";
 
 
 const writeClient = prismic.createWriteClient("linkedbooks", {
-  writeToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoibWFjaGluZTJtYWNoaW5lIiwiZGJpZCI6ImxpbmtlZGJvb2tzLWQzZjE2MDExLWNhMmYtNDhlNC05NzI3LTRhYmU3MmFiYzlkOF81IiwiZGF0ZSI6MTc0MzA4NTk0OCwiZG9tYWluIjoibGlua2VkYm9va3MiLCJhcHBOYW1lIjoid2VicCB0ZXN0IiwiaWF0IjoxNzQzMDg1OTQ4fQ.5QM9nIJ0nELfV1ZsL-eaR9kI4tRgpN9_AW2ShrLZwbc",
+  writeToken: "token",
 });
 
 
